f148b02924b49b2f8409903791c00144b876712c
Kirill Makhalov <zvookiejoo@gmail.com>
2019-01-12 20:34:59 +0500

    App now enumerate files in ZIP archives

---
+++ ExchangeInspector 2.0/zip.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#pragma pack(push, 1)
+typedef struct _ZLocalHeader
+{
+	unsigned int header;
+	unsigned short version;
+	unsigned short bitFlag;
+	unsigned short compMethod;
+	unsigned short lastModTime;
+	unsigned short lastModDate;
+	unsigned int crc32;
+	unsigned int compSize;
+	unsigned int unCompSize;
+	unsigned short fileNameLength;
+	unsigned short extraFieldLength;
+} ZLocalHeader, * PZLocalHeader;
+#pragma pack(pop)
+
+#ifndef ZHEADER
+#define ZHEADER 0x04034B50
+#endif
